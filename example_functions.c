82dc80ac4ebe6462cd8b3fc55a17dd01087cae85
vinicius-r-silva <vinicius.r@usp.br>
2019-10-30 16:28:59 -0300

    created main base code

--- example_functions.c
+++ example_functions.c
@@ -1,45 +1,4 @@
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <ctype.h>
-
-
-
-/*
-* Abaixo seguem funções que fazem a escrita do binário em "stdout" (tela) pra poder ser comparado no run.codes.
-*
-* Funciona assim: você faz tudo o que tiver que fazer na funcionalidade no arquivo em disco, assim como ensinado nas aulas da disciplina.
-* Ao fim da funcionalidade, use a função "binarioNaTela" e a função já cuida de tudo para você. É só chamar a função.
-*
-* Note que ao usar a binarioNaTela1, o fclose no arquivo binário já deve ter sido feito anteriormente. Você passa o nome do arquivo binário ("arquivoTrabX.bin") pra ela e ela vai ler tudo e escrever na tela.
-*
-* Você pode colocar isso num módulo .h separado, ou incluir as funções no próprio código .c: como preferir.
-* VOCÊ NÃO PRECISA ENTENDER ESSAS FUNÇÕES. É só usar elas da forma certa depois de acabar a funcionalidade.
-*
-* Tá tudo testado e funcionando, mas qualquer dúvida acerca dessas funções, falar com o monitor Matheus (mcarvalhor@usp.br).
-*/
-
-
-
-	// Se você for incluir no .h separado, tá abaixo:
-
-	#ifndef H_ESCREVERNATELA_
-	#define H_ESCREVERNATELA_
-	#include <stdio.h>
-
-	void binarioNaTela1(char *nomeArquivoBinario);
-	void trim(char *str);
-	void scan_quote_string(char *str);
-
-	#endif
-
-	// Acabou o código que vai no .h
-
-
-
-// Abaixo vai em algum .c
-
+#include "example_functions.h"
 
 void binarioNaTela1(char *nomeArquivoBinario) {
 
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
+//compiling: gcc -Wall main.c example_functions.c -o main
+
 //Author:
 //Marianna Karenina de Almeida Flores - mariannakarenina@usp.br - 10821144
 //Renan Peres Martins - 107
 //Vinícius Ribeiro da Silva - vinicius.r@usp.br - 10828141
 
 #include <stdio.h>
+#include "example_functions.h"
 
 int main(){
     printf("oi");
-    int z = 0;
     return 0;
 }
