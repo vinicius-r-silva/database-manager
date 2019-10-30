bc2e909651ee47841d60d508e32ee31da0d27e30
vinicius-r-silva <vinicius.r@usp.br>
2019-10-30 14:38:18 -0300

    initial file

---
+++ main.c
@@ -0,0 +1,8 @@
+//Author:
+//Marianna Karenina de Almeida Flores - mariannakarenina@usp.br - 10821144
+//Renan Peres Martins - 107
+//Vinícius Ribeiro da Silva - vinicius.r@usp.br - 10828141
+
+int main(){
+    return 0;
+}
